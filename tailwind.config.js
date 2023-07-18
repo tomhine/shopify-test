819ef122dfa2422b2e133d1f8263a1150cbd778f
Tom Hine <tomhine792@gmail.com>
2023-07-18 21:44:04 +0100

    fix styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./theme/**/*.liquid', './src/**/*.js'],
+  content: ['./**/*.liquid', './src/**/*.js'],
   theme: {
     extend: {},
   },
